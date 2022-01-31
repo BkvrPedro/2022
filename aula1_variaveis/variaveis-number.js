75567735ba55dad5fc1cbd037df2b2c01623ca04
unknown <pedrobbukvar@gmail.com>
2022-01-31 19:16:09 -0300

    LET com number

---
+++ aula1_variaveis/variaveis-number.js
@@ -0,0 +1,8 @@
+let idade = 25;
+console.log(idade);
+
+let alturaEmMetros = 1.89;
+console.log(alturaEmMetros);
+
+//SEMPRE CRIAR VARIAVEIS COM A PRIMEIRA LETRA MINUSCULA, É CONVENCIONADO
+// camelCase = alturaEmMetros / sempreLetrasMaiusculasAPartirDaSegundaPalavra
