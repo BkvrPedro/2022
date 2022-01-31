1bab0bcd46f6373903a2060e4f37301584fcae8b
unknown <pedrobbukvar@gmail.com>
2022-01-31 20:36:08 -0300

    Concatenar strings ou numbers e exercicio 3 nos aritimeticos

--- aula1_variaveis/variaveis-aritimeticos.js
+++ aula1_variaveis/variaveis-aritimeticos.js
@@ -1,7 +1,7 @@
 let x = 10, y = 2;
 
-let resultado = x + y;
-console.log(resultado)      //12
+let resultadoSoma = x + y;
+console.log(resultadoSoma)      //12
 
 resultado = x - y
 console.log(resultado);     //8
@@ -11,3 +11,7 @@ console.log(resultado);     //20
 
 resultado = x / y
 console.log(resultado);     //5
+
+
+//DESTA FORMA, COMEÇAMOS COM ` E DIZEMOS QUE ${X} É 10 E ${Y} É 2, E PEDIMOS O RESULTADO DA SOMA, FECHA`
+console.log(`A soma de ${x} e ${y} é ${resultadoSoma}`)
