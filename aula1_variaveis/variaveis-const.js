30ba3cf7067a50a8f130679e1cdbf10cf23a933d
unknown <pedrobbukvar@gmail.com>
2022-01-31 19:03:20 -0300

    Variaveis  CONST e LET

---
+++ aula1_variaveis/variaveis-const.js
@@ -0,0 +1,15 @@
+// VARIAVEL CONST - QUANDO USADA NAO PODEREMOS MAIS ALTERAR A VARIAVEL "PEDRO" NO CODIGO
+
+const primeironome = "Pedro";
+
+console.log(primeironome);
+
+let segundonome = "Borges"
+
+console.log(segundonome);
+
+segundonome = "bukvar"
+
+console.log(segundonome);
+
+// COM A VARIAVEL LET, APLICANDO O CONSOLE.LOG(PRIMEIRONOME) E DEPOIS O (SEGUNDO NOME) VEREMOS AS DUAS INFOS NO TERMINAL = NOME COMPLETO
