62bee4be224b98b1e0e79fc425d9dde8e09a2428
unknown <pedrobbukvar@gmail.com>
2022-01-31 21:28:16 -0300

    Aritimeticos usando x-= / x+=

---
+++ aula1_variaveis/variaveis-aritimeticos2.js
@@ -0,0 +1,12 @@
+let x = 12
+
+// A CONTA É FEITA DA DIREITA PARA A ESQUERDA, OU SEJA, 3 + X = X ( SE X É 12 ENTÃO 3 + 12 = 15)
+
+// x = x + 3; //15
+// x += 3 // DESSA FORMA DIZEMOS QUE X = X + 3 (IGUAL NO MODELO ACIMA)
+
+// x = x - 3; //9
+// x -= 3; //9
+
+
+console.log(x);
