b706a4e15d0a550dd89c6b26c4b7398d77f00ff4
unknown <pedrobbukvar@gmail.com>
2022-02-03 20:40:19 -0300

    ex6

---
+++ aula2_condicionais/EXERCICIO FINAL/ex6.js
@@ -0,0 +1,19 @@
+const alturaEmCm = 185;
+
+if (alturaEmCm <= 180) {
+    console.log("REPROVADO")
+} else if (alturaEmCm <= 185) {
+    console.log("LIBERO")
+
+} else if (alturaEmCm <= 195) {
+    console.log("PONTEIRO")
+
+} else if (alturaEmCm <= 205) {
+    console.log("OPOSTO")
+
+} else if (alturaEmCm > 205) {
+    console.log("CENTRAL")
+
+} else {
+    console.log("REPROVADO")
+}
