6cb8a81270f278f4d33436ff11cac428a2184485
unknown <pedrobbukvar@gmail.com>
2022-02-03 23:02:07 -0300

    ex11

--- aula2_condicionais/EXERCICIO FINAL/ex11-2.js
+++ aula2_condicionais/EXERCICIO FINAL/ex11-2.js
@@ -1,4 +1,4 @@
-const rendaMensalEmCentavos = 300000;
+const rendaMensalEmCentavos = 150000;
 
 // Tempo decorrido de contrato. Se for maior que 60 meses, o aluno não deve mais nada.
 const mesesDecorridos = 12;
@@ -9,5 +9,5 @@ const totalJaPagoPeloAluno = 1000000;
 if (rendaMensalEmCentavos > 200000 && mesesDecorridos < 60 && totalJaPagoPeloAluno < 1800000) {
     console.log(`O valor da parcela desse mês é R$${rendaMensalEmCentavos * (18 / 100) / 100} reais`)
 } else {
-    console.log("Voce não tem nada a pagar")
+    console.log("O valor da parcela desse mês é R$ 0 reais. Nenhum valor é devido pois a renda do estudante está abaixo do valor mínimo de R$ 2000 reais.")
 }
