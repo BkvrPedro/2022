596720e9b13ffce5ab0486078368af81e7b2bf0f
unknown <pedrobbukvar@gmail.com>
2022-02-07 20:45:58 -0300

    FOR

---
+++ aula3_arrays_e_loops/for.js
@@ -0,0 +1,17 @@
+const paises = ["Brasil", "USA", "China", "Alemanha", "Espanha"]
+
+
+for (let i = 0; i < paises.length; i++) {
+    console.log(paises[i]);
+}
+
+
+
+
+
+// let indice = 0;
+
+// while (indice < paises[paises.length]) {
+//     console.log(paises[indice]);
+//     indice++;
+// }
