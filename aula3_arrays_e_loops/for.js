9b4c6a9800fb483c5ece07e2fdd710bc5d9de32e
unknown <pedrobbukvar@gmail.com>
2022-02-07 20:46:41 -0300

    for

--- aula3_arrays_e_loops/for.js
+++ aula3_arrays_e_loops/for.js
@@ -6,6 +6,10 @@ for (let i = 0; i < paises.length; i++) {
 }
 
 
+//DEIFINIDO QUE i = 0 (PRIMEIRO INDICE)
+// i É MENOR QUE CONTAGEM DO ARRAY DA CONST PAISES?
+// IMPRIMIR O PAIS DO INDICE 0
+// ADICIONA 1 AO INDICE (i++) ... assim por diante
 
 
 
