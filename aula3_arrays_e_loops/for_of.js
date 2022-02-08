cc271a4fe9ea97cca3a4a8a05eca985641026efa
unknown <pedrobbukvar@gmail.com>
2022-02-07 21:27:52 -0300

    For Of

---
+++ aula3_arrays_e_loops/for_of.js
@@ -0,0 +1,7 @@
+const paises = ["Brasil", "USA", "China", "Alemanha", "Espanha"]
+
+for (let variavel of paises) {
+    console.log(variavel)
+}
+
+// SINTAXE: (criar variavel com nome qualquer) = Let "nome" of "NOME ARRAY"
