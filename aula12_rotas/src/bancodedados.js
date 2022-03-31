a3a4a40e9fd0f9d32931651e51fad797666bc285
BkvrPedro <pedrobbukvar@gmail.com>
2022-03-31 20:51:14 -0300

    Aula 12 - Servidor com express

---
+++ aula12_rotas/src/bancodedados.js
@@ -0,0 +1,8 @@
+const usuarios = [
+    { id: 1, nome: "Pedro", stack: "Backend" },
+    { id: 2, nome: "Taina", stack: "Frontend" },
+    { id: 3, nome: "Emerson", stack: "Fullstack" },
+    { id: 4, nome: "Cleide", stack: "Backend" },
+];
+
+module.exports = usuarios
