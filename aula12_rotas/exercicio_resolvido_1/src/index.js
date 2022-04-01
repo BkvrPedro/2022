23b624d0770537e99a0da8496c56cb803f0b5aec
BkvrPedro <pedrobbukvar@gmail.com>
2022-03-31 23:42:41 -0300

    Exercicio resolvio 1 e 2

--- aula12_rotas/exercicio_resolvido_1/src/index.js
+++ aula12_rotas/exercicio_resolvido_1/src/index.js
@@ -4,6 +4,12 @@ const app = express();
 
 const { listagemCarros, obterCarro } = require('./controladores/carros')
 
+const { validarSenha } = require('./intermediarios')
+
+
+app.use(validarSenha)
+
+
 app.get('/carros', listagemCarros);
 app.get('/carros/:id', obterCarro)
 
