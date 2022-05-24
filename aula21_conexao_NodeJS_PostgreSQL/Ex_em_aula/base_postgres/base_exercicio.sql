36e9e0571bcf1728e4adb3befbaa4e2e58335e12
BkvrPedro <pedrobbukvar@gmail.com>
2022-05-23 21:28:27 -0300

    Exercicio resolvido em aula

--- aula21_conexao_NodeJS_PostgreSQL/Ex_em_aula/base_postgres/base_exercicio.sql
+++ aula21_conexao_NodeJS_PostgreSQL/Ex_em_aula/base_postgres/base_exercicio.sql
@@ -23,3 +23,7 @@ CREATE TABLE autores (
  INSERT INTO autores (nome, idade) VALUES
  ('Ambrosio Duarte', 79),
  ('Chico da Silva', 57);
+ 
+ INSERT INTO livros (autor_id, nome, editora, genero, data_publicacao) VALUES
+ (1, 'A volta dos que nao foram', 'Atena', 'Suspense', '02-06-1996'),
+ (2, 'A ida dos que nao vieram', 'Antena', 'Comedia', '06-02-1996');
